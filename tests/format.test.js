15e8b004ac7611d9559ebcbc9d2105ebe5afa146
nora2605 <nora.ja2605@gmail.com>
2024-07-12 02:23:45 +0200

    formatter works well enough

--- tests/format.test.js
+++ tests/format.test.js
@@ -3,4 +3,10 @@ import { format, format_default } from '../lib';
 
 test('format is correct', () => {
     expect(format(`key "value" arr_key [1 2 3]`, format_default)).toBe(`key: "value",\narr_key: [1, 2, 3]`);
+    expect(format(`key "value" obj { key2 "value2" key3 "value3" }`))
+        .toBe(`key: "value",
+obj: {
+    key2: "value2",
+    key3: "value3"
+}`);
 });
